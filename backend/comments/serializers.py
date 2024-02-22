2a8003adc2bc1bac0c40ca07f63aa3de22b9bd88
exp-ext <ext77@yandex.ru>
2024-02-22 09:07:49 +0300

    add comments

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -8,25 +8,13 @@ class UserSerializer(serializers.Serializer):
     email = serializers.EmailField()
 
 
-class RecursiveField(serializers.Serializer):
-    def to_representation(self, value):
-        serializer = self.parent.parent.__class__(value, context=self.context)
-        return serializer.data
-
-
-class CommentSerializer(serializers.Serializer):
+class CommentTreeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     text = serializers.CharField()
-    user = UserSerializer()
     average_rating = serializers.FloatField()
-    children = serializers.ListField(child=RecursiveField(), required=False)
+    user = UserSerializer()
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        comment = representation.pop('comment', None)
-        if comment:
-            user_representation = comment.pop('user', {})
-            representation['user'] = UserSerializer(user_representation).data
-            for key, value in comment.items():
-                representation[key] = value
-        return representation
+    def get_fields(self):
+        fields = super(CommentTreeSerializer, self).get_fields()
+        fields['children'] = CommentTreeSerializer(many=True, required=False)
+        return fields
